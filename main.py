9b53d9c7ea977f75e56c1466dcc7bc1c5686ae9e
ITZ ARCTIX <144344981+teamarctix@users.noreply.github.com>
2024-01-20 15:52:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import yt_dlp
 from pyrogram import Client
+from urllib.parse import urlparse, parse_qs
 
 api_id = "11405252"
 api_hash = "b1a1fc3dc52ccc91781f33522255a880"
@@ -13,22 +14,12 @@ def initialize_client():
 def resolve_channel_id(app, channel_id):
     return app.get_chat(int(channel_id))
 
-def upload_single_video(app, channel_id, folder_path, video):
-    resolved_channel = resolve_channel_id(app, channel_id)
-    video_path = os.path.join(folder_path, video)
-    app.send_video(resolved_channel.id, video_path)
-    print(f"Successfully sent the file: {video}")
-    os.remove(video_path)
-    print(f"Removed the file: {video}")
-
-def upload_videos(app, channel_id, folder_path):
-    videos = [video for video in os.listdir(folder_path) if video.endswith(".mp4")]
-
-    if videos:
-        for video in videos:
-            upload_single_video(app, channel_id, folder_path, video)
-    else:
-        print("No video files found in the folder.")
+def extract_playlist_id(url):
+    parsed_url = urlparse(url)
+    if "playlist" in parsed_url.path:
+        query_params = parse_qs(parsed_url.query)
+        return query_params.get('list', [None])[0]
+    return None
 
 def download_content(url, save_path):
     options = {
@@ -45,7 +36,12 @@ def download_content(url, save_path):
         info_dict = ydl.extract_info(url, download=False)
         
         if 'entries' in info_dict:  # It's a playlist
-            ydl.download([url])
+            playlist_id = extract_playlist_id(url)
+            if playlist_id:
+                ydl.download([url])
+            else:
+                print("No playlist ID found. Downloading the single video.")
+                ydl.download([info_dict['url']])
         else:  # It's a single video
             ydl.download([url])
 
@@ -53,10 +49,9 @@ if __name__ == "__main__":
     app = initialize_client()
 
     folder_path = "Downloads"
-    video_url = 'https://youtube.com/playlist?list=PL3b0A8gfzTYWlc521bm7R2LibJRwcxidd&si=yTYumknOhEIB5ADY'
+    video_url = 'https://www.pornhub.com/playlist/4758401'
 
     download_content(video_url, folder_path)
 
     with app:
         upload_videos(app, channel_id, folder_path)
-        
