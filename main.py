ecfc4610b639a582a45a7b6a4c7583af02ccf0c0
ITZ ARCTIX <144344981+teamarctix@users.noreply.github.com>
2024-01-20 09:03:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ if __name__ == "__main__":
     app = initialize_client()
 
     folder_path = "Downloads"
-    video_url = 'https://www.pornhub.com/playlist/4758401'
+    video_url = 'https://youtube.com/playlist?list=PL3b0A8gfzTYUV1KbrgTHC_9duOHtGjjRc&si=Ff0rTfcpTUEiBAVs'
 
     download_content(video_url, folder_path)
 
